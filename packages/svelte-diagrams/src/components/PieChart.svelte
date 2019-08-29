338d0b0df1c05fffc28e4b100fc2e028818a7ea2
Exybore <exybore@becauseofprog.fr>
2019-08-29 19:03:43 +0200

    feat: add pie chart
    Added the first diagram of the library : pie chart. With only an array
    of numbers and some legends, this component will create a simple but
    nice-looking chart.

---
+++ packages/svelte-diagrams/src/components/PieChart.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let data = []
+  export let legends = []
+  export let size = '150px'
+  export let style = ''
+
+  let total = 0
+  let step = 0
+  style += 'background: conic-gradient('
+
+  data.forEach(function (piece) {
+    total += piece
+  })
+  data.forEach(function(piece, index) {
+    style += `${legends[index]['color']} ${step}deg `
+    step += (piece/total)*360
+    style += `${step}deg,`
+  })
+  style = style.slice(0, -1) + ');'
+
+  style += `width: ${size}; height: ${size};`
+</script>
+
+<style>
+  .diagram.pie-chart {
+    border-radius: 50%;
+  }
+</style>
+
+<div class="diagram pie-chart" {style}>
+
+</div>
